92afe1804c2e387b1f91f3d5575fabab0856119b
Fernando Almeida <fernandogdalmeida@gmail.com>
2023-05-11 16:00:40 -0300

    develop: check balance method added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('hello worlds');
+let accounts = [
+    {
+        id: "1234567",
+        balance: 0
+    }
+]
+
+app.get('/account/balance/', (req, res) => {
+    res.status(400).json({"error" : true, "message" : "Required parameter missing."});
 })
 
+app.get('/account/balance/:id', (req, res) => {
+    // if (!req.params.hasOwnProperty('id'))
+    //     res.status(400).json({"error" : true, "message" : "Required parameter missing."});
+    const acc = accounts.find( a => a.id === req.params.id.replace("-","") );
+    
+    if (!acc)
+        res.status(404).json({"error" : true, "message" : "Account not found"});
+    else
+        res.json({"error" : false, "data" : acc});
+})
 
 app.listen(port, () => {
-    console.log("servidor iniciado");
+    console.log(`servidor iniciado na porta ${port}`);
 })
