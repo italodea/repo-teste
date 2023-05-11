3b1a6e2cf3fefe289d30b3d6bb892dde75104188
Fernando Almeida <fernandogdalmeida@gmail.com>
2023-05-11 19:07:55 -0300

    feature: transfer method added

--- backend/src/index.js
+++ backend/src/index.js
@@ -56,7 +56,7 @@ app.put('/account/credit/', (req, res) => {
         if (!acc) 
             res.status(404).json({"error" : true, "message" : "Account not found."});
         else {
-            acc.balance += parseInt(req.body.value);
+            acc.balance += aux;
             res.json({"error" : false, "data" : acc});
         }
     } else
@@ -74,8 +74,33 @@ app.put('/account/debit/', (req, res) => {
         if (!acc) 
             res.status(404).json({"error" : true, "message" : "Account not found."});
         else {
-            acc.balance -= parseInt(req.body.value);
+            acc.balance -= aux;
             res.json({"error" : false, "data" : acc});
+        }
+    } else
+        res.status(400).json({"error": true, "message": "Required parameter invalid."});
+})
+
+app.put('/account/transfer/', (req, res) => {
+    if (!req.body.hasOwnProperty('originId') || !req.body.hasOwnProperty('destinationId') || !req.body.hasOwnProperty('value'))
+        res.status(400).json({"error": true, "message": "Required parameter missing."});
+
+    if (req.body.originId.replace("-","") === req.body.destinationId.replace("-",""))
+        res.status(400).json({"error": true, "message": "Parameters for 'originId' and 'destinationId' must be different."});
+    
+    const aux = parseFloat(req.body.value);
+
+    if (aux && aux > 0) {
+        const accOrigin = accounts.find( a => a.id === req.body.originId.replace("-","") );
+        const accDestination = accounts.find( a => a.id === req.body.destinationId.replace("-",""));
+        if (!accOrigin) 
+            res.status(404).json({"error" : true, "message" : `Account with id : ${req.body.originId} not found.`});
+        else if (!accDestination)
+            res.status(404).json({"error" : true, "message" : `Account with id : ${req.body.destinationId} not found.`});
+        else {
+            accOrigin.balance -= aux;
+            accDestination.balance += aux;
+            res.json({"error" : false, "data" : {"origin": accOrigin, "destination": accDestination}});
         }
     } else
         res.status(400).json({"error": true, "message": "Required parameter invalid."});
