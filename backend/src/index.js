54c3243b7c63bedc526778994640fd8577b3740d
Fernando Almeida <fernandogdalmeida@gmail.com>
2023-05-11 16:44:07 -0300

    feature: credit method added

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,29 +11,28 @@ let accounts = [
         id: "1234567",
         balance: 0
     }
-]
+];
 
 app.post('/account/create', function (req, res) {
-    if (!req.body.hasOwnProperty('id')){
-        res.status(400).json({"error": true, "message": "The id key was not supplied"});
-    }
-    if (accounts.find(a => a.id === req.body.id.replace("-",""))) {
-        res.status(409).json({"error": true, "message": "The account already exists" });
-    }else{
-        accounts.push({
-            "id": req.body.id.replace("-",""),
-            "balance" : 0
-        });
+    if (!req.body.hasOwnProperty('id'))
+        res.status(400).json({"error": true, "message": "Required parameter missing"});
+
+    if (accounts.find(a => a.id === req.body.id.replace("-",""))) 
+        res.status(409).json({"error": true, "message": "Account already exists." });
+    else {
+        accounts.push(
+            {
+                "id": req.body.id.replace("-",""),
+                "balance" : 0
+            });
     
         res.json({"error": false, "data" : accounts});
     }
 })
 
-
 app.get('/account/balance/', (req, res) => {
     res.status(400).json({"error" : true, "message" : "Required parameter missing."});
-});
-
+})
 
 app.get('/account/balance/:id', (req, res) => {
     // if (!req.params.hasOwnProperty('id'))
@@ -41,9 +40,27 @@ app.get('/account/balance/:id', (req, res) => {
     const acc = accounts.find( a => a.id === req.params.id.replace("-","") );
     
     if (!acc)
-        res.status(404).json({"error" : true, "message" : "Account not found"});
+        res.status(404).json({"error" : true, "message" : "Account not found."});
     else
         res.json({"error" : false, "data" : acc});
+})
+
+app.put('/account/credit/', (req, res) => {
+    if (!req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('value'))
+        res.status(400).json({"error": true, "message": "Required parameter missing."});
+    
+    const aux = parseFloat(req.body.value);
+    console.log(aux);
+    if (aux && aux > 0) {
+        const acc = accounts.find( a => a.id === req.body.id.replace("-","") );
+        if (!acc) 
+            res.status(404).json({"error" : true, "message" : "Account not found."});
+        else {
+            acc.balance += parseInt(req.body.value);
+            res.json({"error" : false, "data" : acc});
+        }
+    } else
+        res.status(400).json({"error": true, "message": "Required parameter invalid."});
 })
 
 app.listen(port, () => {
