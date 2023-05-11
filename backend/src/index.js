0b6ef6c172a92ff109de2cb232c832d8c088bc44
Ítalo de Azevedo Albuquerque <italodeazevedo99@gmail.com>
2023-05-11 20:25:34 -0300

    added sqlite3 support

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,110 +1,268 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var db = require("./database/manage.js")
+
 const app = express();
 app.use(bodyParser.json());
 
-const port = 3000;
 
-let accounts = [
-    {
-        id: "1234567",
-        balance: 0
-    }
-];
+
+const port = 3000;
 
 app.post('/account/create', function (req, res) {
     if (!req.body.hasOwnProperty('id'))
-        res.status(400).json({"error": true, "message": "Required parameter missing"});
-
-    if (accounts.find(a => a.id === req.body.id.replace("-",""))) 
-        res.status(409).json({"error": true, "message": "Account already exists." });
+        res.status(400).json({ "error": true, "message": "Required parameter missing" });
+    // if (accounts.find(a => a.id === req.body.id.replace("-", "")))
+    // res.status(409).json({ "error": true, "message": "Account already exists." });
     else {
-        accounts.push(
-            {
-                "id": req.body.id.replace("-",""),
-                "balance" : 0
-            });
-    
-        res.json({"error": false, "data" : accounts});
+        req.body.id += "";
+
+        var sql = "select * from accounts where account = ?"
+        var params = [req.body.id.replace('-', '')]
+
+        db.get(sql, params, (err, row) => {
+            if (row) {
+                res.status(503).json({ "error": true, "message": "Account already exists." });
+            } else {
+                var sql = 'INSERT INTO accounts (account, balance) VALUES (?,?)';
+                db.run(sql, [req.body.id.replace("-", ""), 0], (err) => {
+                    if (err) {
+                        res.status(503).json({ "error": true, "message": "Cannot query database" });
+                    } else {
+                        var acc = {
+                            "id": req.body.id.replace("-", ""),
+                            "balance": 0
+                        }
+                        res.json({ "error": false, "data": acc });
+                    }
+                })
+            }
+        });
     }
 })
 
-app.get('/account/balance/', (req, res) => {
-    res.status(400).json({"error" : true, "message" : "Required parameter missing."});
-})
 
 app.get('/account/balance/:id', (req, res) => {
-    // if (!req.params.hasOwnProperty('id'))
-    //     res.status(400).json({"error" : true, "message" : "Required parameter missing."});
-    const acc = accounts.find( a => a.id === req.params.id.replace("-","") );
-    
-    if (!acc)
-        res.status(404).json({"error" : true, "message" : "Account not found."});
-    else
-        res.json({"error" : false, "data" : acc});
+
+    // Check if the req has the parameter id
+    if (!req.params.hasOwnProperty('id'))
+        res.status(400).json({ "error": true, "message": "Required parameter missing." });
+
+
+    var sql = "select * from accounts where account = ?"
+    var params = [req.params.id.replace('-', '')]
+
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(503).json({ "error": true, "message": "Cannot query database" });
+        } else {
+            if (row) {
+                const acc = {
+                    "id": row.account,
+                    "balance": row.balance
+                };
+                res.json({ "error": false, "data": acc });
+            } else {
+                res.status(404).json({ "error": true, "message": "Account not found." });
+            }
+        }
+    });
+
+
 })
 
 app.put('/account/credit/', (req, res) => {
     if (!req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('value'))
-        res.status(400).json({"error": true, "message": "Required parameter missing."});
-    
-    const aux = parseFloat(req.body.value);
-    
-    if (aux && aux > 0) {
-        const acc = accounts.find( a => a.id === req.body.id.replace("-","") );
-        if (!acc) 
-            res.status(404).json({"error" : true, "message" : "Account not found."});
-        else {
-            acc.balance += aux;
-            res.json({"error" : false, "data" : acc});
+        res.status(400).json({ "error": true, "message": "Required parameter missing." });
+
+
+    var sql = "select * from accounts where account = ?";
+    //convert id into string if it comes without the '-'
+    req.body.id += ""
+    var ammount = parseFloat(req.body.value)
+
+    if (!(ammount && ammount > 0))
+        res.status(400).json({ "error": true, "message": "Required parameter invalid." });
+
+
+    var params = [req.body.id.replace("-", "")];
+
+    /**
+     * Get the account by the account number and then run the update with new balance
+     */
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(503).json({ "error": true, "message": "Cannot query database" });
+        } else {
+            if (row) {
+                var balance = row.balance + ammount;
+                var sql = "update accounts set balance = ? where account = ? "
+                var params = [balance, req.body.id.replace("-", "")]
+
+                db.run(sql, params, (err) => {
+                    if (err) {
+                        res.status(503).json({ "error": true, "message": "Cannot query database" });
+                    }
+                });
+                const acc = {
+                    "id": row.account,
+                    "balance": balance
+                };
+                res.json({ "error": false, "data": acc });
+            } else {
+                res.status(404).json({ "error": true, "message": "Account not found." });
+            }
         }
-    } else
-        res.status(400).json({"error": true, "message": "Required parameter invalid."});
+    });
+
+
 })
 
 app.put('/account/debit/', (req, res) => {
     if (!req.body.hasOwnProperty('id') || !req.body.hasOwnProperty('value'))
-        res.status(400).json({"error": true, "message": "Required parameter missing."});
-    
-    const aux = parseFloat(req.body.value);
+        res.status(400).json({ "error": true, "message": "Required parameter missing." });
 
-    if (aux && aux > 0) {
-        const acc = accounts.find( a => a.id === req.body.id.replace("-","") );
-        if (!acc) 
-            res.status(404).json({"error" : true, "message" : "Account not found."});
-        else {
-            acc.balance -= aux;
-            res.json({"error" : false, "data" : acc});
+
+    var sql = "select * from accounts where account = ?";
+    //convert id into string if it comes without the '-'
+    req.body.id += ""
+    var ammount = parseFloat(req.body.value)
+
+    if (!(ammount && ammount > 0))
+        res.status(400).json({ "error": true, "message": "Required parameter invalid." });
+
+
+    var params = [req.body.id.replace("-", "")];
+
+    /**
+     * Get the account by the account number and then run the update with new balance
+     */
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(503).json({ "error": true, "message": "Cannot query database" });
+        } else {
+            if (row) {
+                var balance = row.balance - ammount;
+                var sql = "update accounts set balance = ? where account = ? "
+                var params = [balance, req.body.id.replace("-", "")]
+
+                db.run(sql, params, (err) => {
+                    if (err) {
+                        res.status(503).json({ "error": true, "message": "Cannot query database" });
+                    }
+                });
+                const acc = {
+                    "id": row.account,
+                    "balance": balance
+                };
+                res.json({ "error": false, "data": acc });
+            } else {
+                res.status(404).json({ "error": true, "message": "Account not found." });
+            }
         }
-    } else
-        res.status(400).json({"error": true, "message": "Required parameter invalid."});
+    });
 })
 
 app.put('/account/transfer/', (req, res) => {
     if (!req.body.hasOwnProperty('originId') || !req.body.hasOwnProperty('destinationId') || !req.body.hasOwnProperty('value'))
-        res.status(400).json({"error": true, "message": "Required parameter missing."});
+        res.status(400).json({ "error": true, "message": "Required parameter missing." });
 
-    if (req.body.originId.replace("-","") === req.body.destinationId.replace("-",""))
-        res.status(400).json({"error": true, "message": "Parameters for 'originId' and 'destinationId' must be different."});
-    
-    const aux = parseFloat(req.body.value);
+    if (req.body.originId.replace("-", "") === req.body.destinationId.replace("-", ""))
+        res.status(400).json({ "error": true, "message": "Parameters for 'originId' and 'destinationId' must be different." });
 
-    if (aux && aux > 0) {
-        const accOrigin = accounts.find( a => a.id === req.body.originId.replace("-","") );
-        const accDestination = accounts.find( a => a.id === req.body.destinationId.replace("-",""));
-        if (!accOrigin) 
-            res.status(404).json({"error" : true, "message" : `Account with id : ${req.body.originId} not found.`});
-        else if (!accDestination)
-            res.status(404).json({"error" : true, "message" : `Account with id : ${req.body.destinationId} not found.`});
-        else {
-            accOrigin.balance -= aux;
-            accDestination.balance += aux;
-            res.json({"error" : false, "data" : {"origin": accOrigin, "destination": accDestination}});
+
+
+    var sql = "select * from accounts where account = ?";
+    //convert id into string if it comes without the '-'
+    req.body.originId += ""
+    req.body.destinationId += ""
+
+    var ammount = parseFloat(req.body.value)
+
+    if (!(ammount && ammount > 0))
+        res.status(400).json({ "error": true, "message": "Required parameter invalid." });
+
+
+    var params = [req.body.originId.replace("-", "")];
+
+    // get the origin account
+    db.get(sql, params, (err, rowOrigin) => {
+        if (err) {
+            res.status(503).json({ "error": true, "message": "Cannot query database" });
+        } else {
+            if (rowOrigin) {
+
+                //get the destination account
+                params = [req.body.destinationId.replace("-", "")];
+                db.get(sql, params, (err, rowDestination) => {
+
+                    if (err) {
+                        res.status(503).json({ "error": true, "message": "Cannot query database" });
+                    } else {
+
+                        // check if origin is ok
+                        if (!rowOrigin) {
+                            res.status(404).json({ "error": true, "message": "Origin account not found." });
+                        }
+                        // check if destination is ok
+                        else if (!rowDestination) {
+                            res.status(404).json({ "error": true, "message": "Destination account not found." });
+                        }
+
+                        else {
+
+                            // debit from the origin account
+                            var balance = rowOrigin.balance - ammount;
+                            var sql = "update accounts set balance = ? where account = ? "
+                            var params = [balance, rowOrigin.account]
+
+                            db.run(sql, params, (err) => {
+                                if (err) {
+                                    res.status(503).json({ "error": true, "message": "Cannot query database" });
+                                }
+                            });
+
+
+                            const acc = [{
+                                "id": rowOrigin.account,
+                                "balance": balance
+                            }];
+
+                            // credit in the destination account
+                            balance = rowDestination.balance + ammount;
+                            sql = "update accounts set balance = ? where account = ? "
+                            params = [balance, rowDestination.account]
+
+                            db.run(sql, params, (err) => {
+                                if (err) {
+                                    res.status(503).json({ "error": true, "message": "Cannot query database" });
+                                }
+                            });
+
+                            acc.push({
+                                "id": rowDestination.account,
+                                "balance": balance
+                            });
+
+                            res.json({ "error": false, "data": acc });
+                        }
+                    }
+                });
+            } else {
+                res.status(404).json({ "error": true, "message": `Account with id : ${req.body.originId} not found.` });
+            }
         }
-    } else
-        res.status(400).json({"error": true, "message": "Required parameter invalid."});
+    });
 })
+
+
+
+
+// Default response for any other request
+app.use(function (req, res) {
+    res.status(404);
+});
 
 app.listen(port, () => {
     console.log(`servidor iniciado na porta ${port}`);
